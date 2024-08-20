f9829ac99c2af29a75f0c7aabd15b2f21c2f6255
Ryan Morales <ryan.morales@west-mec.org>
2024-08-20 11:25:09 -0700

    removed inner http

--- basic_node_server/index.js
+++ basic_node_server/index.js
@@ -3,14 +3,6 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    if (req.url === '/' && req.method === 'GET') {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.end('hello you found the home page');
-    }
-    else {
-        res.writeHead(404, { 'Content-Type': 'text/html' });
-        res.end('404 resource not found');
-    }
 });
 
 const port = 3000;
