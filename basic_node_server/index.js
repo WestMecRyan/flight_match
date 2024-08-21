116c43c424c8ce47f7990ed30a7b1032ed6fd45e
Ryan Morales <ryan.morales@west-mec.org>
2024-08-21 11:20:42 -0700

    end step-3 added multilpe endpoints and error handling for 404

--- basic_node_server/index.js
+++ basic_node_server/index.js
@@ -1,12 +1,23 @@
 const http = require('http');
 const port = 3000;
 const server = http.createServer((req, res) => {
-    // set the http status code of the response to 200 ok
-    res.statusCode = 200;
-    // set a content type of text/plain which tells the clielt the type of data being sent
-    res.setHeader('Content-Type', 'text/plain');
-    // the server ends the cycle by res(ponding) with a string of hello world and a new line break \n
-    res.end('Hello, World!\n');
+    // make an if statement to handle the GET request at an endpoint
+    if (req.method === 'GET' && req.url === '/') {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('Hello, World!\n');
+    }
+
+    else if (req.method === 'GET' && req.url === '/about') {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/html');
+        res.end('<h1>About Me</h1>');
+    } else {
+        res.statusCode = 404;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('Not Found');
+    }
 });
 server.listen(port, () => {
-console.log(`Server running at ${port}`);});
+    console.log(`Server running at ${port}`);
+});
