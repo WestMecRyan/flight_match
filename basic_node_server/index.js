f84c5d688516279f186cb9fc1eb59a263fe98c7d
Ryan Morales <ryan.morales@west-mec.org>
2024-08-21 11:37:38 -0700

    end step-4 created sanitized html

--- basic_node_server/index.js
+++ basic_node_server/index.js
@@ -1,7 +1,8 @@
 const http = require('http');
+// install and require sanitize-html for sanitizing the html sent back to the client
+const sanitizeHtml = require('sanitize-html');
 const port = 3000;
 const server = http.createServer((req, res) => {
-    // make an if statement to handle the GET request at an endpoint
     if (req.method === 'GET' && req.url === '/') {
         res.statusCode = 200;
         res.setHeader('Content-Type', 'text/plain');
@@ -9,9 +10,20 @@ const server = http.createServer((req, res) => {
     }
 
     else if (req.method === 'GET' && req.url === '/about') {
+        // unsanitize html that could come from user input to server
+        let unsanitizedHtml = '<h1>About Me</h1><script>alert(`xss`)</script>'
+        // modify the unsanitized html to sanitized html
+        let sanitizedHtml = sanitizeHtml(unsanitizedHtml, {
+            // allowedTags: ['h1', 'p', 'b', 'i', 'em', 'strong', 'a'],
+            // allowedAttributes: {
+            //     'a': ['href']
+            // }
+        });
         res.statusCode = 200;
         res.setHeader('Content-Type', 'text/html');
-        res.end('<h1>About Me</h1>');
+        // add an xss script to demonstrate how cross site scripting can add malicious code
+        // res.end(sanitizedHtml);
+        res.end(unsanitizedHtml);
     } else {
         res.statusCode = 404;
         res.setHeader('Content-Type', 'text/plain');
